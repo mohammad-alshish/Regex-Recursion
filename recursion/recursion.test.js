cad4b4f92df796512bc69b5c76b15f5d1bdaad47
Mohammad majed alshish <eng.mohammed.majed@gmail.com>
2022-05-04 20:08:44 +0300

    DONE

--- recursion/recursion.test.js
+++ recursion/recursion.test.js
@@ -1,7 +1,6 @@
 /* Write a function to do the division operation without using the built-in division*/
 
-function division(number, dividedBy){
-    // Write you logic here.
+function division(number, dividedBy) {
     if (number == 0 || dividedBy == 0) {
         return 0;
     } else if (number === dividedBy) {
@@ -11,39 +10,43 @@ function division(number, dividedBy){
     }
 }
 
-
 /* Write a function that implement Math.pow(x,n) but using recursion
 Example:
 pow(2,4) = 16
 */
 
 
-function pow(x,n){
-    // Write you logic here.
-    function pow(x, n) {
-        if (n == 0) {
-            return 1;
-        } else {
-            return x * pow(x, n - 1);
-        }
+function pow(x, n) {
+    if (n == 0) {
+        return 1;
+    } else {
+        return x * pow(x, n - 1);
     }
-}    
+}
 
 /* The Fibonacci Series is a numeric series starting with the integers 0 and 1. In this series,
 the next integer is determined by summing the previous two. This gives us:
+
 0, 1, 1, 2, 3, 5, 8, 13, ...
+
 Write a function that take n as parameter and return the nth element in the Fibonacci Series
+
 Example: n = 4 ==> 3, n= 0 ==> 0, n = 3 ==> 2 */
 
-function fibonacci(n){
-    // Write you logic here.
-    if (n <= 1)
-    return n;
-return fibonacci(n-1) + fibonacci(n-2);
+function fibonacci(n) {
+    if (n == 1 || n == 0) {
+        return n;
+    } else {
+        return fibonacci(n - 1) + fibonacci(n - 2);
+    }
 }
+
 /* Optional 
+
 The set [1, 2, 3, ..., n] contains a total of n! unique permutations.
+
 By listing and labeling all of the permutations in order, we get the following sequence for n = 3:
+
 "123"
 "132"
 "213"
@@ -51,11 +54,12 @@ By listing and labeling all of the permutations in order, we get the following s
 "312"
 "321"
 Given n and k, return the kth permutation sequence.
+
 Example: 
 Input: n = 3, k = 3
 Output: ["123", "132", "213", "231", "312", "321"] */
 
-function permutations(n,k){
+function permutations(n, k) {
     let arr = [];
     // Write you logic here. 
     return arr
@@ -64,18 +68,18 @@ function permutations(n,k){
 
 describe("Test division", () => {
     test("Return the division result", () => {
-        expect(division(10,2)).toStrictEqual(5);
-        expect(division(10,0)).toStrictEqual(0);
-        expect(division(0,10)).toStrictEqual(0);
-        
+        expect(division(10, 2)).toStrictEqual(5);
+        expect(division(10, 0)).toStrictEqual(0);
+        expect(division(0, 10)).toStrictEqual(0);
+
     })
 });
 
 describe("Test pow", () => {
     test("It should work as Math.pow(x,n)", () => {
-        expect(pow(10,2)).toStrictEqual(100);
-        expect(pow(10,0)).toStrictEqual(1);
-        expect(pow(0,0)).toStrictEqual(1);
+        expect(pow(10, 2)).toStrictEqual(100);
+        expect(pow(10, 0)).toStrictEqual(1);
+        expect(pow(0, 0)).toStrictEqual(1);
     })
 });
 
@@ -91,7 +95,7 @@ describe("Test fibonacci", () => {
 
 describe("Test permutations", () => {
     test("It should return a list of possible combinations", () => {
-        expect(permutations(3,3)).toStrictEqual(["123", "132", "213", "231", "312", "321"]);
-        expect(permutations(3,0)).toStrictEqual([]);
+        expect(permutations(3, 3)).toStrictEqual(["123", "132", "213", "231", "312", "321"]);
+        expect(permutations(3, 0)).toStrictEqual([]);
     })
 });
